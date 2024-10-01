329c2c59364776ce302f7fcc3252cb8348da1d66
SantoshReddyPasunooru <92775271+SantoshReddyPasunooru@users.noreply.github.com>
2024-10-01 15:59:48 -0500

    Initial commit: Added basic React app structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <header className="App-header">
+          <h1>Welcome to My First React App</h1>
+          <p>This is a basic React app for demonstration purposes.</p>
+        </header>
+      </div>
   );
 }
 
