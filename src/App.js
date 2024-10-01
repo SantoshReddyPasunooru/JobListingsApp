c8973221395d46769e1263c5315b110651c86375
SantoshReddyPasunooru <92775271+SantoshReddyPasunooru@users.noreply.github.com>
2024-10-01 16:03:25 -0500

    Modified App.js to display Hello World

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-      <div className="App">
-        <header className="App-header">
-          <h1>Welcome to My First React App</h1>
-          <p>This is a basic React app for demonstration purposes.</p>
-        </header>
-      </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>Hello World</h1>
+                <p>This is my modified React app.</p>
+            </header>
+        </div>
+    );
 }
 
 export default App;
