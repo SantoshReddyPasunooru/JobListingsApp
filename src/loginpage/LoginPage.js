65fe922f7d40b712922085298bdfcb6e71833913
SantoshReddyPasunooru <92775271+SantoshReddyPasunooru@users.noreply.github.com>
2024-10-02 13:03:53 -0500

    Just able to create box, shadows for that, background image, need to design the login page inside but need to learn margins, paddings first

--- src/loginpage/LoginPage.js
+++ src/loginpage/LoginPage.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import './LoginPage.css';
 
 function LoginPage() {
     return (
         <div>
-            <h1>Login Page</h1>
-            <p>Welcome! Please log in.</p>
+            <form className="loginform">
+                    <text className="loginDetailstitle"> Login Details</text>
+                <div>
+                    <text className="username">Username :</text>
+                    <input type={"text"} className="usernameinput" placeholder={"Enter Username"} />
+                </div>
+
+
+            </form>
         </div>
     );
 }
